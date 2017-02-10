d1d154da9e790cc1d83de99e6e1e5236adbf9dc8
rrgayhart <rrgayhart@gmail.com>
2017-02-10 12:26:22 -0700

    refactors

---
+++ GroupDialog.js
@@ -0,0 +1,82 @@
+var readline = require('readline');
+var shuffleArray = require('./shuffleArray.js');
+
+function GroupDialog(students) {
+  this.rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  this.students = students;
+  this.groups = [];
+}
+
+GroupDialog.prototype.startGroupCountLoop = function() {
+  this.rl.question('How Many Students? \n', this.getGroupCount.bind(this));
+};
+
+GroupDialog.prototype.startAcceptanceLoop = function(splicedStudents){
+  this.rl.question('Is this group okay?\n (y|n)\n',
+                    this.handleAcceptanceResponse.bind(this, splicedStudents));
+};
+
+GroupDialog.prototype.getGroupCount = function(answer){
+  if (!answer.match(/\d+/))  {
+    this.rl.close();
+  } else {
+    this.num = parseInt(answer);
+    this.startGroupApprovalLoop();
+  }
+};
+
+GroupDialog.prototype.startGroupApprovalLoop = function() {
+  if (this.students.length){
+    this.students = shuffleArray(this.students);
+    var splicedStudents = this.spliceStudents();
+    logStudents(splicedStudents);
+    this.startAcceptanceLoop(splicedStudents);
+  } else {
+    logGroups(this.groups);
+    this.rl.close();
+  }
+};
+
+GroupDialog.prototype.handleAcceptanceResponse = function(students, answer){
+  if (answer === 'y') {
+    this.groups.push(students);
+    this.startGroupApprovalLoop(this.num);
+  } else {
+    this.returnToStudentBatch(students);
+    this.startGroupApprovalLoop(this.num);
+  }
+};
+
+GroupDialog.prototype.spliceStudents = function(){
+  var splicedStudents = [];
+  for (var i = 0; i < this.num; i++){
+    var s = this.students.pop();
+    splicedStudents.push(s);
+    if (!this.students.length) {
+      break;
+    }
+  }
+  return splicedStudents;
+};
+
+GroupDialog.prototype.returnToStudentBatch = function(splicedStudents){
+  Array.prototype.push.apply(this.students, splicedStudents);
+};
+
+function logGroups(groups){
+  groups.forEach(function(group){
+    console.log('');
+    logStudents(group);
+  });
+}
+
+function logStudents(group){
+  group.forEach(function(s){
+    console.log('* ' + s);
+  });
+}
+
+module.exports = GroupDialog;
--- index.js
+++ index.js
@@ -1,81 +1,13 @@
 #!/usr/bin/env node
-var readline = require('readline');
-var shuffleArray = require('./shuffleArray.js');
-var students = require('./data/students.js');
+var fs = require('fs');
 
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+var GroupDialog = require('./GroupDialog.js');
+
+fs.readFile('./data/1608.txt', function (err, data) {
+  if (err) {
+    return console.error(err);
+  }
+  students = data.toString().split('\n');
+  var groupDialog = new GroupDialog(students);
+  groupDialog.startGroupCountLoop();
 });
-
-var groups = [];
-
-function startGroupCountLoop(){
-  rl.question('How Many Students? \n', getGroupCount);
-}
-
-function getGroupCount(answer){
-  if (answer.match(/[quit|q]/)) {
-    rl.close();
-  } else if (!answer.match(/\d+/))  {
-    startGroupCountLoop();
-  } else {
-    var num = parseInt(answer);
-    startGroupApprovalLoop(num);
-  }
-}
-
-function startGroupApprovalLoop(num){
-  if (students.length){
-    students = shuffleArray(students);
-    var splicedStudents = spliceStudents(num);
-    logStudents(splicedStudents);
-    startAcceptanceLoop(splicedStudents, num);
-  } else {
-    logGroups(groups);
-    rl.close();
-  }
-}
-
-function startAcceptanceLoop(splicedStudents, num){
-  rl.question('Is this group okay?\n (y|n)\n', function(answer){
-    if (answer === 'y') {
-      groups.push(splicedStudents);
-      startGroupApprovalLoop(num);
-    } else {
-      returnToStudentBatch(splicedStudents);
-      startGroupApprovalLoop(num);
-    }
-  });
-}
-
-function spliceStudents(num){
-  var splicedStudents = [];
-  for (var i = 0; i < num; i++){
-    var s = students.pop();
-    splicedStudents.push(s);
-    if (!students.length) {
-      break;
-    }
-  }
-  return splicedStudents;
-}
-
-function returnToStudentBatch(splicedStudents){
-  Array.prototype.push.apply(students, splicedStudents);
-}
-
-function logGroups(groups){
-  groups.forEach(function(group){
-    console.log('');
-    logStudents(group);
-  });
-}
-
-function logStudents(group){
-  group.forEach(function(s){
-    console.log('* ' + s);
-  });
-}
-
-startGroupCountLoop();
